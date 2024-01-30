72eac6ced7ca40e315fd77aa46f909c1e06513ae
avilagabriella <avilagabriella84@gmail.com>
2024-01-30 17:49:23 -0300

    aulas

--- Banco/script.sql
+++ Banco/script.sql
@@ -1,15 +1,26 @@
 drop database if exists cantina;
 create database cantina;
 use cantina;
-create table Cantina(
-    id_salgado int(10) not null auto_increment,
-    nome varchar(50) not null,
-    preço decimal(10,2), 
-    atendente int(10),
-     
 
-    primary key (id_salgado)
+create table Salgados(
+
+    nome varchar(50) not null,
+    produtos varchar(100),
+    valor decimal(10, 2) not null,
+    quantidade int not null,
+    total decimal(10, 2) not null,
+    data datetime not null
+
 );
 
-select * from Cantina
- 
+describe Salgados;
+
+insert into Salgados( nome, produtos, valor, quantidade, total, data)
+values
+
+( "Igor", "Produto a", 7.50, "1", 7.50, "2024-01-30"),
+( "Iagora", "Produto b", 4,00, "1", 4.00, "2024-01-30"),
+( "Ana", "Produto c", 3.50, "1", 3.50, "2024-01-30");
+
+
+select * from Salgados;
